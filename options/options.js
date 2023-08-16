f47708e85d2a0e521740a128461c01aa02752abd
Crowdbotics <team@crowdbotics.com>
2023-08-16 17:26:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-butterfly-42883",
+  url: "https://late-butterfly-42883.botics.co",
+  api: "https://late-butterfly-42883.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
